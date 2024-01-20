c8f59647f4975fc5c52b1f0dcc593fd5dd671c5a
Martins <vanagsm4@gmail.com>
2024-01-20 20:17:11 +0200

    Switch out the root path

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->id('admin')
-            ->path('admin')
+            ->path('/')
             ->colors([
                 'primary' => Color::Amber,
             ])
